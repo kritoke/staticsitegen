5c8ba7c1f686edfe903f69336fc0413e8192c6a0
kritoke <3081486+kritoke@users.noreply.github.com>
2025-04-18 10:39:36 -0500

    Add htmlnode.

--- src/main.py
+++ src/main.py
@@ -1,7 +1,11 @@
 from textnode import TextNode
+from htmlnode import HTMLNode
 
 def main():
     new_text = TextNode("This is some anchor text", "link", "https://www.boot.dev")
     print(new_text)
+
+    new_html = HTMLNode(None, None, None, '{ "href": "https://www.google.com", "target": "_blank", }')
+    print(new_html.props_to_html())
 
 main()
