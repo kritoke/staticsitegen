d68ddd3c90cd673a3d34bb2c8d010a5203d5b628
kritoke <3081486+kritoke@users.noreply.github.com>
2025-05-12 05:33:41 -0500

    Add text node to html functionality with tests

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -29,5 +29,48 @@ class TestTextNode(unittest.TestCase):
         node2 = TextNode("This is a test", TextType.BOLD, None)
         self.assertEqual(node, node2)
 
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = node.text_node_to_html_node()
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_bold(self):
+        node = TextNode("This is a bold text", TextType.BOLD)
+        html_node = node.text_node_to_html_node()
+        self.assertEqual(html_node, "**This is a bold text**")
+
+    def test_italic(self):
+        node = TextNode("This is an italic text", TextType.ITALIC)
+        html_node = node.text_node_to_html_node()
+        self.assertEqual(html_node, "_This is an italic text_")
+
+    def test_code(self):
+        node = TextNode("This is a code text", TextType.CODE)
+        html_node = node.text_node_to_html_node()
+        self.assertEqual(html_node, "`This is a code text`")
+
+    def test_links(self):
+        node = TextNode("This is a link", TextType.LINKS, "https://example.com")
+        html_node = node.text_node_to_html_node()
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a link")
+
+    def test_image(self):
+        node = TextNode("This is an image", TextType.IMAGE, "https://example.com/image.jpg")
+        html_node = node.text_node_to_html_node()
+        self.assertEqual(html_node, "  This is an image  ")
+
+    def test_empty_text(self):
+        node = TextNode("", TextType.TEXT)
+        html_node = node.text_node_to_html_node()
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "")
+
+    def test_empty_bold(self):
+        node = TextNode("", TextType.BOLD)
+        html_node = node.text_node_to_html_node()
+        self.assertEqual(html_node, "****")
+
 if __name__ == "__main__":
     unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,12 +1,13 @@
 from enum import Enum
+from leafnode import LeafNode
 
 class TextType(Enum):
-    NORMAL = ""
+    TEXT = ""
     BOLD = "**"
     ITALIC = "_"
     CODE = "`"
     LINKS = ""
-    Images = "  "
+    IMAGE = "  "
 
 class TextNode():
     def __init__(self, text, text_type, url = None):
@@ -22,4 +23,9 @@ class TextNode():
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
-    
+
+    def text_node_to_html_node(self):
+        if self.text_type == TextType.TEXT or self.text_type == TextType.LINKS:
+            return LeafNode(None, self.text)
+        else:
+            return f"{self.text_type.value}{self.text}{self.text_type.value}"
